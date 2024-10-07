6f3fcdb904a31ad2331abc97a35f6e480cff6cb7
Cear-CAAR <cesar.andres5242@gmail.com>
2024-10-07 14:57:56 -0700

    cambio

--- lista.cs
+++ lista.cs
@@ -55,5 +55,41 @@ namespace AppLIsta
                 }
             }
         }
+
+        public int Count()
+        {
+            //regresar el numero de nodos en la lista
+            //regresar 0 si la lista esta vacia
+        }
+
+        public int Find(int pos)
+        {
+            //regresar el valor del nodo en la posición
+            //indica, la primera posición es cero (como arreglo)
+            //si no esta la posición, regresar -1
+        }
+
+        public int FindValue(int num)
+        {
+            //regresar la posición del nodo del valor indicado
+            //la primera posición siempre es cero (como arreglo)
+            //si no esta el valor, regresa -1
+        }
+
+        public bool Change(int pos)
+        {
+            //cambia el valor dol nodo en la posicion indicada
+            //y regresa true si el cambio fue exitoso
+            //si no encuentra el nodo o la lista esta vacia
+            //regresar false, ya que el cambio no fue exitoso
+        }
+
+        public bool Delete(int pos)
+        {
+            //elimina el nodo en la posicion indicada
+            //regresa true si la eliminacion fue exitosa
+            //regresa false si la eliminacion no fue exitosa
+            //si la lista esta vacia o no esta la posicion
+        }
     }
 }
