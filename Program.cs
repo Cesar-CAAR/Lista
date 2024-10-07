98c6548fd94fa31d40cef99e71dd2dde7d1f0588
Cear-CAAR <cesar.andres5242@gmail.com>
2024-10-07 14:44:58 -0700

    lista

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLIsta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            lista miLista = new lista();
+            miLista.Add(4);
+            miLista.Add(5);
+            miLista.Add(9);
+            miLista.Add(12);
+
+            miLista.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lista.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLIsta
+{
+    internal class lista
+    {
+        private Nodo inicio;
+
+        public lista()
+        {
+            inicio = null;
+        }
+
+        //Metodo para insertar nodo 
+        public void Add(int num)
+        {
+            Nodo nuevo = new Nodo(num);
+
+
+            if (inicio == null)
+            {
+                inicio = nuevo;
+            }
+            else
+            {
+                Nodo act = inicio;
+                while (act.Sig != null)
+                {
+                    act = act.Sig;
+                }
+                act.Sig = nuevo;
+            }
+        }
+
+
+        public void Print()
+        {
+            Nodo act;
+            act = inicio;
+
+            if (act == null)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                while (act != null)
+                {
+                    Console.WriteLine($"{act.Valor}");
+                    act = act.Sig;
+                }
+            }
+        }
+    }
+}
